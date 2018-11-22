976e2e637d58c06996a49e9fb6f57b58978dc8a0
Dorieke Berends <dorieke90@gmail.com>
2018-11-22 11:12:05 +0100

    Defined relationships between users, wishlists and wishes

--- app/Wish.php
+++ app/Wish.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Wish extends Model
 {
     protected $guarded = [];
+
+    public function wishlist() {
+        return $this->belongsTo('App\Wishlist');
+    }
 }
--- app/Wishlist.php
+++ app/Wishlist.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Wishlist extends Model
 {
     protected $guarded = [];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function wishes() {
+        return $this->hasMany('App\Wish');
+    }
 }
