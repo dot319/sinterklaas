9be8dffe71038b19f1f63539c48b213cf3bf974c
Dorieke Berends <dorieke90@gmail.com>
2018-11-22 09:47:12 +0100

    Created layout for wishlists/edit

--- resources/views/wishlists/edit.blade.php
+++ resources/views/wishlists/edit.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts/app')
 
 @section('content')
-Edit wishlist
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+            <h1 class="card-title text-center">Wishlist title</h1>
+            <div class="card mb-4">
+                <div class="card-body">
+                    Letter to sinterklaas
+                </div>
+            </div>
+            <div class="card">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        Item 1
+                    </li>
+                    <li class="list-group-item">
+                        Item 2
+                    </li>
+                    <li class="list-group-item">
+                        Add new item
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
