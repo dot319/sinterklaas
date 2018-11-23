5cadc0e77edbf7e05f97c0b1f95e11a759c99547
Dorieke Berends <dorieke90@gmail.com>
2018-11-23 11:41:53 +0100

    Added hyperlinks to new wishlist and wishlist index to user homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Welkom {{ Auth::user()->name }}!</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,19 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div class="mb-3">Wat wil je doen?</div>
+                    <div class="d-inline-block">
+                        <div class="mb-3">
+                            <a href="/wishlists/store">
+                                <button class="btn btn-primary btn-lg btn-block">Maak een nieuw lijstje</button>
+                            </a>                           
+                        </div>
+                        <div>
+                            <a href="/wishlists">
+                                <button class="btn btn-primary btn-lg btn-block">Bekijk jouw lijstjes</button>
+                            </a>                            
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
